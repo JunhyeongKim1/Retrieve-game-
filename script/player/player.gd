74eede16993e94e60b05a1c4a9e3436d8479fc90
junhyeongKim <kjh8087123@gmail.com>
2026-04-08 14:27:16 +0900

    플레이어 애니메이션, 상태 연결

--- script/player/player.gd
+++ script/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
@@ -10,13 +9,18 @@ const JUMP_VELOCITY = -500.0
 var coyote_timer = 0.0
 var jump_buffer_timer = 0.0
 
+# 상태 정의
+enum State { IDLE, RUN, JUMP, FALL }
+var current_state: State = State.IDLE
+
+@onready var anim = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 중력
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	#Coyote time 갱신
+	# Coyote Time 갱신
 	if is_on_floor():
 		coyote_timer = coyote_time
 	else:
@@ -34,12 +38,43 @@ func _physics_process(delta: float) -> void:
 		jump_buffer_timer = 0
 		coyote_timer = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# 이동
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.flip_h = direction < 0  # 좌우 반전
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# 상태 갱신 (move_and_slide 이후에 해야 is_on_floor()가 정확)
+	_update_state()
+	
+	
+
+func _update_state() -> void:
+	var new_state: State
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			new_state = State.JUMP
+		else:
+			new_state = State.FALL
+	elif velocity.x != 0:
+		new_state = State.RUN
+	else:
+		new_state = State.IDLE
+
+	# 상태가 바뀔 때만 애니메이션 교체
+	if new_state != current_state:
+		current_state = new_state
+		_play_animation()
+
+func _play_animation() -> void:
+	match current_state:
+		State.IDLE: anim.play("idle")
+		State.RUN:  anim.play("run")
+		State.JUMP: anim.play("jump")
+		State.FALL: anim.play("fall")
+	print("Animation Change" + str(current_state))
